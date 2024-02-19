15811ef845d213ebff1a6d12f754e902b6db93bc
quality-assurance01 <qa-1@qwertysytem.net>
2024-02-19 17:21:47 +0700

    adding module license maintenance

---
+++ Object Repository/Menu license maintenance/Object0702_tambah addon asset.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Object0702_tambah addon asset</name>
+   <tag></tag>
+   <elementGuidId>078559d1-3454-4176-af39-0f25899c18d3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[1]/div/div[2]/div[2]/div/div/div[3]/div/div/div[2]/div[2]/div[1]/div[2]/button[7]/span[2]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Menu titik 3 license maintenance/Object0707_ya hapus addon asset maintenance.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Object0707_ya hapus addon asset maintenance</name>
+   <tag></tag>
+   <elementGuidId>97c9f31c-47db-4f35-a4c6-9193bdb5d4c9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[10]/div/div[2]/div/div[4]/button[2]/span</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
