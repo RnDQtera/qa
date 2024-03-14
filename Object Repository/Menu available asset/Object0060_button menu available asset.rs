dee40f3e243991aa11859eab5f77c34a1d3a694e
quality-assurance01 <qa-1@qwertysytem.net>
2024-03-14 15:51:50 +0700

    module available

--- Object Repository/Menu available asset/Object0060_button menu available asset.rs
+++ Object Repository/Menu available asset/Object0060_button menu available asset.rs
@@ -7,7 +7,7 @@
    <selectorCollection>
       <entry>
          <key>XPATH</key>
-         <value>/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div/div[1]/div[2]/ul/li[2]/div/a/span</value>
+         <value>/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div/div[1]/div[2]/ul/li[2]/div/a</value>
       </entry>
       <entry>
          <key>BASIC</key>
